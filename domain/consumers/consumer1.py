83cd9c08616fa1120922c837c4dbfea3464a4c51
Adrian Jasinski <BoTongHua@protonmail.com>
2025-04-28 17:43:57 +0200

    update

--- domain/consumers/consumer1.py
+++ domain/consumers/consumer1.py
@@ -1,5 +1,5 @@
 import time
-from core.events import Type1Event
+from domain.events import Type1Event
 from infrastructure.messaging import RabbitMQConnection, RabbitMQConsumer
 from publishers.publisher1 import setup_logger
 
